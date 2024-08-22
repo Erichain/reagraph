78cd9164e65e267d18d81728c9260cb03704122d
Erichain <erichainzain@gmail.com>
2024-08-22 16:26:59 +0800

    Update

--- src/symbols/Label.tsx
+++ src/symbols/Label.tsx
@@ -77,6 +77,7 @@ export const Label: FC<LabelProps> = ({
         color={normalizedColor}
         fillOpacity={opacity}
         textAlign="center"
+        characters={text}
         outlineWidth={stroke ? 1 : 0}
         outlineColor={normalizedStroke}
         depthOffset={0}
